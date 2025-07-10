19fbb662d4cf766de6d9f8b75383b3b8805f516d
Bhuwan-Shahi <shahibhuwan265@gmail.com>
2025-07-10 18:47:27 +0545

    Techthon 1.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Smart Citizen Portal',
+  description: 'Techthon 1.0',
+  generator: 'Bhuwan Shahi',
 }
 
 export default function RootLayout({
